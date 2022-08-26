f884ea82459e89174bb3a1ec8cb02c7942457d86
miyakawa.kazuya <c.kazunon2020@gmail.com>
2022-08-26 22:50:48 +0900

    バックエンド機能を追加

---
+++ server/fake-db.js
@@ -0,0 +1,78 @@
+const Product = require('./model/product')
+
+class FakeDb {
+	constructor() {
+		this.products = [
+			{
+				coverImage: './assets/image/phone-cover.jpg',
+				name: 'Phone XL',
+				price: 799,
+				description: 'A large phone with one of the best screens',
+				heading1: 'サンプルテキスト1',
+				heading2: 'サンプルテキスト2',
+				heading3: 'サンプルテキスト3',
+				headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+				headingText2: 'サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章',
+				headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			},
+			{
+				coverImage: './assets/image/phone-cover.jpg',
+				name: 'Phone Mini',
+				price: 699,
+				description: 'A great phone with one of the best cameras',
+				heading1: 'サンプルテキスト1',
+				heading2: 'サンプルテキスト2',
+				heading3: 'サンプルテキスト3',
+				headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+				headingText2: 'サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章',
+				headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			},
+			{
+				coverImage: './assets/image/phone-cover.jpg',
+				name: 'Phone Standard',
+				price: 299,
+				description: '',
+				heading1: 'サンプルテキスト1',
+				heading2: 'サンプルテキスト2',
+				heading3: 'サンプルテキスト3',
+				headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+				headingText2: 'サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章',
+				headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			},
+			{
+				coverImage: './assets/image/phone-cover.jpg',
+				name: 'Phone Special',
+				price: 999,
+				description: '',
+				heading1: 'サンプルテキスト1',
+				heading2: 'サンプルテキスト2',
+				heading3: 'サンプルテキスト3',
+				headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+				headingText2: 'サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章 サンプル文章',
+				headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			}
+		]
+	}
+
+	async initDb() {
+		await this.cleanDb()
+		this.pushProductsToDb()
+	}
+
+	async cleanDb() {
+		await Product.deleteMany({})
+	}
+
+	pushProductsToDb() {
+		this.products.forEach((product) => {
+				const newProduct = new Product(product)
+				newProduct.save()
+		})
+	}
+
+	seeDb() {
+		this.pushProductsToDb()
+	}
+}
+
+module.exports = FakeDb
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/dev')
+const FakeDb = require('./fake-db')
+const productRoutes = require('./routes/products')
+
+// MongoDBに接続
+mongoose.connect(config.DB_URI, { 
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+}).then(() => {
+	// ダミーデータで初期化
+	const fakeDb = new FakeDb()
+	fakeDb.initDb()
+})
+
+const app = express()
+app.use('/api/v1/products', productRoutes)
+
+const PORT = process.env.PORT || '3001'
+app.listen(PORT, function() {
+	console.log('I am running!')
+})
